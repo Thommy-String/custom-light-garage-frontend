5d868ad37c10740fec9645ffec765cfa4b122f20
Thomas Dascalu <thomasdascalu@Mac-mini-di-Thomas.local>
2025-07-14 19:09:06 +0200

    Move frontend to root and configure docs deploy

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  
+  root: 'frontend',
+ 
+  base: '/custom-light-garage-frontend/',
+  build: {
+  
+    outDir: '../docs',
+  
+    emptyOutDir: true
+  },
+  plugins: [react()],
+})
